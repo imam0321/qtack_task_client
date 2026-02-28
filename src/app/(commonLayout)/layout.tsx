d2693c51a1c0fa7117bc525cb7cefdf0cfad10ba
imam0321 <imam.hossain0321@gmail.com>
2026-03-01 01:25:52 +0600

    project setup

---
+++ src/app/(commonLayout)/layout.tsx
@@ -0,0 +1,17 @@
+import Footer from "@/components/shared/Footer";
+import Navbar from "@/components/shared/Navbar";
+
+
+export default function CommonLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
+  );
+}
